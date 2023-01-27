c88fdf9e58122b30cff017ec0265a2cf624da0c1
twnj <ltk09@yndx.ru>
2023-01-28 02:03:43 +0300

    add requirements and change .gitignore, settings.py

--- good_website/good_website/settings.py
+++ good_website/good_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'true'
 
-ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
+ALLOWED_HOSTS = []
 
 
 # Application definition
